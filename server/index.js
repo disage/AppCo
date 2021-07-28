d3ad3198f225fa63c9138469b28bf93214838800
disage <krechetov52@gmail.com>
2021-07-28 11:03:55 +0300

    fix route

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3004
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
 	app.use(express.static('client/build'));​
-	app.get('*', (req, res) => {
+	app.get('*', (req, res) => {	
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
